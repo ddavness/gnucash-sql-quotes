55332ba763c5ab16bb7003229d40052b6491d506
David <david@duqued.net>
2021-11-27 01:53:19 +0000

    SQL Driver stubs

---
+++ SQLDriver/PgSQLDriver.py
@@ -0,0 +1,4 @@
+import psycopg
+
+class PgSQLDriver():
+    pass
---
+++ SQLDriver/__init__.py
@@ -0,0 +1,5 @@
+# Generic SQL adapter for multiple drivers
+
+from .MySQLDriver import MySQLDriver as MySQL
+from .PgSQLDriver import PgSQLDriver as PgSQL
+from .SQLiteDriver import SQLiteDriver as SQLite
