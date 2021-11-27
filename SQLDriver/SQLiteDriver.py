5b333e49d150eef5711a1c8ec772b72fd4f2adae
David <david@duqued.net>
2021-11-27 02:12:50 +0000

    sqlite3 driver

--- SQLDriver/SQLiteDriver.py
+++ SQLDriver/SQLiteDriver.py
@@ -1,4 +1,24 @@
 import sqlite3
 
 class SQLiteDriver():
-    pass
+    target = None
+
+    def __init__(self, file = ":memory:") -> None:
+        self.target = file
+    
+    def __enter__(self):
+        self.cnx = sqlite3.connect(self.target)
+        return self
+
+    def __exit__(self, _et, _ev, _tb):
+        self.cnx.commit()
+        self.cnx.close()
+        self.cnx = None
+
+    def execute(self, query = "", params = tuple()) -> list:
+        if self.cnx:
+            raise ValueError("Connection not established yet!")
+        
+        with self.cnx.cursor() as c:
+            c.execute(query, params)
+            return c.fetchall()
