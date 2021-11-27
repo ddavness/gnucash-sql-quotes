3cb1be71691fb4962c2aeb42656702d6ee110808
David <david@duqued.net>
2021-11-27 01:53:54 +0000

    Postgres driver

--- SQLDriver/PgSQLDriver.py
+++ SQLDriver/PgSQLDriver.py
@@ -1,4 +1,31 @@
 import psycopg
 
 class PgSQLDriver():
-    pass
+    cnx = None
+
+    def __init__(self, host = "localhost", database = None, user = None, pw = None, port = 5432) -> None:
+        super().__init__()
+        userspec = f"{user}:{pw}@" if user is not None and pw is not None \
+            else (f"{user}@" if user is not None else "")
+        if database is None:
+            raise ValueError("No database specified!")
+
+        self.url = f"postgresql://{userspec}{host}:{port}/{database}"
+    
+    def __enter__(self):
+        self.cnx = psycopg.connect(self.url)
+        return self
+
+    def __exit__(self, _et, _ev, _tb):
+        self.cnx.close()
+        self.cnx = None
+
+    def execute(self, query = "", params = tuple()) -> list:
+        if self.cnx:
+            raise ValueError("Connection not established yet!")
+        
+        with self.cnx.cursor() as c:
+            c.execute(query, params)
+            dump = c.fetchall()
+            c.commit()
+            return dump
