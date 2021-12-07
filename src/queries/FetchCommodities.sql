48022f6ca2daef4dd44bf6172d017071cf87f775
David <david@duqued.net>
2021-12-07 23:25:18 +0000

    Include commodity namespace

--- src/queries/FetchCommodities.sql
+++ src/queries/FetchCommodities.sql
@@ -1,3 +1,4 @@
-SELECT c.mnemonic, c.guid, qc.currency_guid FROM commodities c, quote_currencies qc
+SELECT c.mnemonic, c.guid, c.namespace, qc.currency_guid
+FROM commodities c, quote_currencies qc
     WHERE c.quote_source = 'quotes.py'
     AND c.guid = qc.guid;
