838fbdbba2f5b034cbf1fc7b61397023044e784c
David <david@duqued.net>
2021-12-07 23:34:54 +0000

    Include currency mnemonic

--- src/queries/FetchCommodities.sql
+++ src/queries/FetchCommodities.sql
@@ -1,4 +1,5 @@
-SELECT c.mnemonic, c.guid, c.namespace, qc.currency_guid
-FROM commodities c, quote_currencies qc
+SELECT c.namespace, c.mnemonic, cc.mnemonic, c.guid, qc.currency_guid 
+FROM commodities c, commodities cc, quote_currencies qc
     WHERE c.quote_source = 'quotes.py'
-    AND c.guid = qc.guid;
+    AND c.guid = qc.commodity_guid
+    AND cc.guid = qc.currency_guid;
